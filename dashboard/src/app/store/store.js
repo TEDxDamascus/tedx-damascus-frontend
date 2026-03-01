e5e35e961108a06b3439666de8de5929c3a05aa3
esraakanat <kntesraa82@gmail.com>
2026-03-02 01:46:23 +0300

    feat: add i18n setup and LocaleInput component

--- dashboard/src/app/store/store.js
+++ dashboard/src/app/store/store.js
@@ -3,12 +3,14 @@ import { setupListeners } from '@reduxjs/toolkit/query';
 import apiService from './apiService';
 import userReducer from '../auth/store/userSlice';
 import localeReducer from './localeSlice';
+import autocompleteReducer from '../shared-components/custom-autocomplete/autocompleteSlice';
 
 export const store = configureStore({
   reducer: {
     [apiService.reducerPath]: apiService.reducer,
     user: userReducer,
-    locale: localeReducer
+    locale: localeReducer,
+    autocomplete: autocompleteReducer
   },
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware().concat(apiService.middleware)
