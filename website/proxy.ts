cb13106f1c155ec4f3b40c43c2f208306aea9b24
SedrAlex <sedralexblockchain@gmail.com>
2026-06-04 22:55:05 +0300

    fix the route

--- website/proxy.ts
+++ website/proxy.ts
@@ -1,15 +1,7 @@
-import { createNavigation } from 'next-intl/navigation';
 import { defineRouting } from 'next-intl/routing';
-import createMiddleware from 'next-intl/middleware';
 
 export const routing = defineRouting({
   locales: ['en', 'ar'],
   defaultLocale: 'en',
   localePrefix: 'always',
 });
-
-export default createMiddleware(routing);
-
-export const config = {
-  matcher: ['/', '/(ar|en)/:path*', '/((?!api|_next|_vercel|.*\\..*).*)'],
-};
