cdb1ee6365a12d1acfcd1becb9e301b91e7b05b0
Sedra Mhanna <Sedra@seventechnologies.net>
2026-07-31 10:45:08 +0300

    fix the translation

--- website/app/[locale]/team/page.tsx
+++ website/app/[locale]/team/page.tsx
@@ -1,6 +1,7 @@
 import { setRequestLocale } from 'next-intl/server';
 import { routing } from '@/routing';
-import ComingSoonClient from '../coming-soon/ComingSoonClient';
+import { TeamPage } from '@/components/team/TeamPage';
+import { Footer } from '@/components/layout/Footer';
 
 export function generateStaticParams() {
   return routing.locales.map((locale) => ({ locale }));
@@ -10,8 +11,13 @@ type Props = {
   params: Promise<{ locale: string }>;
 };
 
-export default async function TeamPage({ params }: Props) {
+export default async function Team({ params }: Props) {
   const { locale } = await params;
   setRequestLocale(locale);
-  return <ComingSoonClient locale={locale} />;
+  return (
+    <main className="relative">
+      <TeamPage locale={locale} />
+      <Footer locale={locale} />
+    </main>
+  );
 }
